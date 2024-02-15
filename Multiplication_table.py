d98d1c64c6972281ae80d5bf393d967eca97648d
Agbolade Elijah <agboladeelijah05@gmail.com>
2024-02-15 23:29:53 +0100

    Multiplication Table

---
+++ Multiplication_table.py
@@ -0,0 +1,33 @@
+"""Multiplication Table, by AI Sweigart modified by Agbolade Elijah
+Print a multiplication table.
+Tags: tiny, begineer, math
+"""
+
+print("Multiplication Table, modified by Agbolade Elijah(Python learner)")
+
+# Print the horizotal number labels:
+print('  |  0  1   2   3   4   5   6   7   8   9   10  11  12')
+print('--+---------------------------------------------------')
+
+# Display each row of products:
+for number1 in range(0, 13):
+    # print(number1 , end=" ")
+    
+    # Print the vertical numbers labels:
+    print(str(number1).rjust(2) ,end='')
+    
+    # Print a seperating bar:
+    print('|', end='')
+    
+    for number2 in range(0, 13):
+        # Print the product followed by a space.
+        print(str(number1 * number2).rjust(3), end=' ')
+    print()
+    
+    
+
+        
+
+    
+
+    
